3d2ba63c153549816df11c40620c03dc74f105fd
Yves Tscherry <info@feerglas.com>
2023-07-09 20:58:08 +0200

    add minimal lang switcher

--- src/app/[locale]/page.jsx
+++ src/app/[locale]/page.jsx
@@ -1,12 +1,21 @@
 import styles from './page.module.css'
 import {useTranslations} from 'next-intl';
+import Link from 'next/link';
 
 export default function Home() {
   const t = useTranslations('Index');
 
   return (
     <main className={styles.main}>
-      {t('title')}
+      <p>{t('title')}</p>
+      <ul>
+        <li>
+          <Link href="/de">Deutsch</Link>
+        </li>
+        <li>
+          <Link href="/en">English</Link>
+        </li>
+      </ul>
     </main>
   )
 }
