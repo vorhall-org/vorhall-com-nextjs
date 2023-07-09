f6c75a456ac3dee4c5008a9ea7388a75b173fdbf
Yves Tscherry <info@feerglas.com>
2023-07-09 20:49:28 +0200

    use correct next-intl  property for passing translations

--- src/i18n.js
+++ src/i18n.js
@@ -1,5 +1,5 @@
 import {getRequestConfig} from 'next-intl/server';
  
 export default getRequestConfig(async ({locale}) => ({
-  translations: (await import(`./translations/${locale}.json`)).default
+  messages: (await import(`./translations/${locale}.json`)).default
 }));
