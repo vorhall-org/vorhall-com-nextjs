02d9e40b603f895cc38e36debaf296a6b721eea7
Yves Tscherry <info@feerglas.com>
2023-07-11 07:06:19 +0200

    don't handle favicon by middleware

--- middleware.js
+++ middleware.js
@@ -8,7 +8,6 @@ function getLocale(request) {
   const negotiatorHeaders = {}
   request.headers.forEach((value, key) => (negotiatorHeaders[key] = value))
 
-  // @ts-ignore locales are readonly
   const locales = i18n.locales
 
   // Use negotiator and intl-localematcher to get best locale
@@ -26,14 +25,9 @@ export function middleware(request) {
 
   // // `/_next/` and `/api/` are ignored by the watcher, but we need to ignore files in `public` manually.
   // // If you have one
-  // if (
-  //   [
-  //     '/manifest.json',
-  //     '/favicon.ico',
-  //     // Your other files in `public`
-  //   ].includes(pathname)
-  // )
-  //   return
+  if (['/favicon.ico',].includes(pathname)) {
+    return;
+  }
 
   // Check if there is any supported locale in the pathname
   const pathnameIsMissingLocale = i18n.locales.every(
