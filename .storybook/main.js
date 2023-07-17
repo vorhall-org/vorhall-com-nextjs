559b66b1bce22199f501e3847bf00544eb840338
Yves Tscherry <info@feerglas.com>
2023-07-17 15:51:34 +0200

    add default story main file

---
+++ .storybook/main.js
@@ -0,0 +1,21 @@
+/** @type { import('@storybook/nextjs').StorybookConfig } */
+const config = {
+  stories: [
+    "../**/*.mdx",
+    "../**/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+    "@storybook/addon-a11y",
+  ],
+  framework: {
+    name: "@storybook/nextjs",
+    options: {},
+  },
+  docs: {
+    autodocs: "tag",
+  },
+};
+export default config;
