332f9177845cae0ec17948f51ed16c717dccb5c8
feerglas <info@feerglas.com>
2023-08-29 17:42:54 +0200

    add some sample links

--- app/nav-data.js
+++ app/nav-data.js
@@ -18,6 +18,30 @@ const navData = async (locale) => {
       text: dictionary.navigation.services,
       link: 'services'
     },
+    {
+      text: 'test lorem',
+      link: 'test'
+    },
+    {
+      text: 'test2 ipsum',
+      link: 'test2'
+    },
+    {
+      text: 'test3 lorem',
+      link: 'test3'
+    },
+    {
+      text: 'test4 ipsum',
+      link: 'test4'
+    },
+    {
+      text: 'test5 lorem',
+      link: 'test5'
+    },
+    {
+      text: 'test6 lorem',
+      link: 'test6'
+    },
   ];
 }
 
