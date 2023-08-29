5ee995218e29b20995a8664ea0b0e06e11c35246
feerglas <info@feerglas.com>
2023-08-29 17:43:13 +0200

    add markup and logic to handle nav button on mobile

--- app/components/nav/nav.js
+++ app/components/nav/nav.js
@@ -4,25 +4,72 @@ import Link from 'next/link';
 import styles from './nav.module.scss';
 import { useState } from 'react';
 import clsx from 'clsx';
-import NavButton from '../nav-button/nav-button';
+import Image from 'next/image';
+import openIcon from '../../../public/icons/menu.svg';
+import closeIcon from '../../../public/icons/close.svg';
 
 export default function Nav({ items, strings }) {
+  // ------------------
+  // State
+  // ------------------
+
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  // ------------------
+  // Event Handlers
+  // ------------------
+
+  function handleMenuClick() {
+    setMenuOpen(!menuOpen);
+  }
 
   // ------------------
   // Render
   // ------------------
+  const menuListId = 'menu-list';
 
   return (
     <nav className={styles.nav}>
-      <NavButton strings={strings} />
+      <div className={styles.logo}>LOGO</div>
+
+      <button
+        className={
+          clsx(
+            styles.menuButton,
+            menuOpen ? styles.menuButtonOpen : styles.menuButtonClosed
+          )
+        }
+        aria-expanded={menuOpen}
+        aria-controls={menuListId}
+        onClick={handleMenuClick}
+      >
+
+        <span className={styles.menuButtonHiddenText}>
+          {
+            menuOpen
+              ? strings.closeNavigation
+              : strings.openNavigation
+          }
+        </span>
+
+        <Image
+          priority
+          className={styles.menuButtonIcon}
+          aria-hidden
+          role="presentation"
+          src={menuOpen ? closeIcon : openIcon}
+          alt=""
+        />
+      </button>
+
       <ul
         className={
           clsx(
             styles.list,
-            true ? styles.listVisible : ''
+            menuOpen ? styles.listMobile : ''
           )
         }
-        id="menu-list"
+        id={menuListId}
         role="menu"
       >
         {items.map((item) => {
